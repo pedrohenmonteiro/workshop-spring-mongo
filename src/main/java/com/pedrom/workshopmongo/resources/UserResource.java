30779598b1ded97105c2aeaa6c2be0d4f8aea21d
pedrohenmonteiro <pedromont436@gmail.com>
2023-02-20 23:01:08 -0300

    Repository and Service

--- src/main/java/com/pedrom/workshopmongo/resources/UserResource.java
+++ src/main/java/com/pedrom/workshopmongo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.pedrom.workshopmongo.resources;
 
 import com.pedrom.workshopmongo.domain.User;
+import com.pedrom.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +13,18 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(value="/users")
 public class UserResource {
+
+    @Autowired
+    private UserService service;
 
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
         User u1 = new User("1", "Maria Brown", "maria@gmail.com");
         User u2 = new User("2", "Alex Green", "alex@gmail.com");
 
-        List list = new ArrayList();
-        list.addAll(Arrays.asList(u1, u2));
+        List list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
