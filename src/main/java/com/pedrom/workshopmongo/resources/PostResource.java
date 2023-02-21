54dc03d4c854495a3ef73155fe2164766fe15b1f
pedrohenmonteiro <pedromont436@gmail.com>
2023-02-21 13:22:26 -0300

    getting posts by id

---
+++ src/main/java/com/pedrom/workshopmongo/resources/PostResource.java
@@ -0,0 +1,34 @@
+package com.pedrom.workshopmongo.resources;
+
+import com.pedrom.workshopmongo.domain.Post;
+import com.pedrom.workshopmongo.domain.User;
+import com.pedrom.workshopmongo.dto.UserDTO;
+import com.pedrom.workshopmongo.services.PostService;
+import com.pedrom.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
+
+}
---
+++ src/main/java/com/pedrom/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.pedrom.workshopmongo.services;
+
+import com.pedrom.workshopmongo.domain.Post;
+import com.pedrom.workshopmongo.domain.User;
+import com.pedrom.workshopmongo.dto.UserDTO;
+import com.pedrom.workshopmongo.repository.PostRepository;
+import com.pedrom.workshopmongo.repository.UserRepository;
+import com.pedrom.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+
+}
